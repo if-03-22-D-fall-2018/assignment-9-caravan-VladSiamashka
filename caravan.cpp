362793d82d772796e17910a8ad1aab074c207a9d
SiamashkaVlad <wladsemashko@gmail.com>
2019-01-09 19:28:20 +0100

    dont know how optimize_load works

--- caravan.cpp
+++ caravan.cpp
@@ -10,34 +10,141 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include <stdlib.h>
+#include "caravan.h"
+#include "general.h"
+
+#define CAMEL_MAX_SPEED 20
+#define HORSE_MAX_SPEED 50
+
+typedef struct NodeImplementation* Node;
+
+struct CaravanImplementation
+{
+  int length;
+  Node head;
+};
+struct NodeImplementation
+{
+  struct NodeImplementation* next;;
+  PackAnimal animal;
+};
+
+
 Caravan new_caravan()
 {
+  Caravan new_caravan = (Caravan)malloc(sizeof(struct CaravanImplementation));
+  new_caravan->head = 0;
+  new_caravan->length = 0;
+  return new_caravan;
 }
 
 int get_length(Caravan caravan)
 {
+  return caravan->length;
+
 }
 
 void delete_caravan(Caravan caravan)
 {
+  Node current = caravan->head;
+  while (current != 0)
+  {
+    Node to_delate = current;
+    current = current->next;
+    sfree(to_delate);
+  }
+
+  sfree(caravan);
 }
 
 void add_pack_animal(Caravan caravan, PackAnimal animal)
 {
+  if (animal != 0)
+  {
+    Node current = caravan->head;
+    while (current!=0) {
+      if (current->animal==animal)return;
+      current=current->next;
+    }
+    current = caravan->head;
+    Node new_node = (Node)malloc(sizeof(struct NodeImplementation));
+    new_node->animal = animal;
+    new_node->next = 0;
+
+    if (caravan->head==0) {
+      caravan->head=new_node;
+      caravan->length++;
+      add_to_caravan(animal,caravan);
+    }
+    else{
+      while (current->next!=0) {
+        current=current->next;
+      }
+      current->next=new_node;
+      caravan->length++;
+      add_to_caravan(animal,caravan);
+    }
+  }
+
 }
 
 void remove_pack_animal(Caravan caravan, PackAnimal animal)
 {
+  Node current = caravan->head;
+  if (animal!=0) {
+    if (current!=0&&current->animal==animal) {
+      caravan->head=current->next;
+      sfree(current);
+      remove_from_caravan(animal,caravan);
+      caravan->length--;
+    }
+    while (current->next!=0&&current->next->animal!=animal) {
+      current=current->next;
+    }
+    if (current->next!=0) {
+      current->next=current->next->next;
+      sfree(current->next);
+      caravan->length--;
+      remove_from_caravan(animal,caravan);
+    }
+  }
+
 }
 
 int get_caravan_load(Caravan caravan)
 {
+  int load=0;
+  Node current=caravan->head;
+  while (current!=0) {
+    load+=get_load(current->animal);
+    current=current->next;
+  }
+  return load;
 }
 
 void unload(Caravan caravan)
 {
+  Node current=caravan->head;
+  while (current!=0) {
+    unload(current->animal);
+    current=current->next;
+  }
 }
 
 int get_caravan_speed(Caravan caravan)
 {
+  int speed=1000000;
+  Node current=caravan->head;
+  while (current!=0) {
+    if (get_actual_speed(current->animal)<speed) {
+      speed=get_actual_speed(current->animal);
+    }
+    current=current->next;
+  }
+  return speed;
+}
+void optimize_load(Caravan caravan)
+{
+
 }
